24edc769490fb7e753700fd3a2ef1b823bb7f996
Guillaume RICHARD <g.jf.richard@gmail.com>
2017-10-16 09:35:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>menu demo</title>
+		<link rel="stylesheet" href="http://code.jquery.com/ui/1.11.3/themes/ui-darkness/jquery-ui.css">
+		<link rel="stylesheet" href="common/css/style.css">
+	</head>
+
+	<body>		
+		<ul id="menu">
+			<li><a href="#">Item 1</a></li>
+			<li><a href="#">Item 2</a>
+				<ul>
+					<li><a href="#">Item 2-1</a></li>
+					<li><a href="#">Item 2-2</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Item 3</a>
+				<ul>
+					<li><a href="#">Item 3-1</a></li>
+					<li><a href="#">Item 3-2</a></li>
+					<li><a href="#">Item 3-3</a></li>
+					<li><a href="#">Item 3-4</a></li>
+					<li><a href="#">Item 3-5</a></li>
+				</ul>
+			</li>
+			<li><a href="#">Item 4</a>
+				<ul>
+					<li><a href="#">Item 4-1</a></li>
+					<li><a href="#">Item 4-2</a></li>
+					<li><a href="#">Item 4-3</a></li>
+				</ul>
+			<li><a href="#">Item 5</a></li>
+		</ul>
+
+		<script src="http://code.jquery.com/jquery-1.11.2.js"></script>
+		<script src="http://code.jquery.com/ui/1.11.3/jquery-ui.js"></script>
+		<script src="common/js/script.js"></script>
+	</body>
+</html>
